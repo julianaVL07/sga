48c42c3fad91d0d1d2061ba7ed5044da3c2f36ac
julianaVL07 <juliana.villotal@uqvirtual.edu.co>
2026-08-31 11:40:02 -0500

    Primer commit

---
+++ sga/src/main/java/co/edu/uniquindio/sga/SgaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.sga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgaApplication.class, args);
+	}
+
+}
---
+++ sga/src/test/java/co/edu/uniquindio/sga/SgaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.sga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
